36e94950cdbf50513a38441c7e0e7b097ffa81ba
danielbfalk <dbfalk@ucsd.edu>
2025-03-09 15:35:19 -0700

    Update connection.py

--- app/database/connection.py
+++ app/database/connection.py
@@ -9,7 +9,7 @@ def get_db_connection():
     try:
         connection = mysql.connector.connect(
             host=os.getenv('MYSQL_HOST'),
-            port=int(os.getenv('MYSQL_PORT')),
+            port=int(os.getenv('DB_PORT', 20212)),#port=int(os.getenv('MYSQL_PORT')),
             user=os.getenv('MYSQL_USER'),
             password=os.getenv('MYSQL_PASSWORD'),
             database=os.getenv('MYSQL_DATABASE'),
